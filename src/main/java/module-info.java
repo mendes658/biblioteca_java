d2cdfb9a3e572d1d4559d67a5f490fc7686eb80d
Pedro Mendes <pmo2002@gmail.com>
2023-08-06 13:19:03 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.pbl.biblioteca {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.pbl.biblioteca.view to javafx.fxml;
+    exports com.pbl.biblioteca.view;
+    exports com.pbl.biblioteca;
+    opens com.pbl.biblioteca to javafx.fxml;
+}
