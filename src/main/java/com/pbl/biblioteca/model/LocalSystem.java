eb5bd0176ae5866795dfe1e890eda5e910336dbd
Pedro Mendes <pmo2002@gmail.com>
2023-10-02 00:09:33 -0300

    docs: adding LocalSystem javadoc

--- src/main/java/com/pbl/biblioteca/model/LocalSystem.java
+++ src/main/java/com/pbl/biblioteca/model/LocalSystem.java
@@ -1,8 +1,7 @@
 package com.pbl.biblioteca.model;
 
 import com.pbl.biblioteca.dao.DAO;
-import com.pbl.biblioteca.exceptionHandler.notFoundException;
-import com.pbl.biblioteca.exceptionHandler.wrongPasswordException;
+import com.pbl.biblioteca.exceptionHandler.*;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -11,6 +10,10 @@ import java.util.HashMap;
 
 public class LocalSystem implements Serializable {
 
+    /**
+     * Atualiza as reservas, deletando as que possuem usuários bloqueados, ou que já bateram no prazo
+     * Seta a data final das reservas que já possuem cópias livres para empréstimo
+     */
     public static void updateReserves(){
         HashMap<String, ArrayList<BookReserve>> allReserves = DAO.getBookReserveDAO().getAllByBook();
         Book nowBook;
@@ -47,6 +50,9 @@ public class LocalSystem implements Serializable {
         }
     }
 
+    /**
+     * Atualiza os usuários, liberando o bloqueio daqueles que já cumpriram o prazo
+     */
     public static void updateReadersBlockStatus(){
         HashMap<String, Reader> all = DAO.getReaderDAO().getAll();
         Reader nowReader;
@@ -63,12 +69,23 @@ public class LocalSystem implements Serializable {
         }
     }
 
-
+    /**
+     * Atualiza os usuários e as reservas
+     */
     public static void updateSystem(){
         updateReadersBlockStatus();
         updateReserves();
     }
 
+    /**
+     * Loga um usuário
+     * @param  username Username do usuário
+     * @param  password Senha do usuário
+     * @param  type Tipo do usuário (librarian, admin ou reader)
+     * @return Retorna o objeto User
+     * @throws wrongPasswordException Caso a senha seja inválida
+     * @throws notFoundException Caso não exista um usuário de mesmo username e tipo
+     */
     public static User login(String username, String password, String type) throws
             wrongPasswordException, notFoundException{
 
